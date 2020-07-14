ef02e76382d9b162f0da76090dfafb0f838f2bb4
saiyam <Saiyam@LAPTOP-DPOGCFLB>
2020-07-14 19:25:17 +0530

    my first commit

---
+++ secondproject/testapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+from django.http import HttpResponse
+
+# Create your views here.
+def date_time_view(request):
+    date=datetime.datetime.now()
+    s='<h1>the current date and time is at server is:'+ str(date)+'<h1>'
+    return HttpResponse(s)
